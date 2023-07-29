d218bc253370e0e76dd38564a3fba632739e059f
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-07-29 00:19:25 -0500

    Feat: Serializacion de los objetos
    Se agrego la serializacion de los objetos para poder ser enviados en sockets como bytes

---
+++ LocalEndpointManager_InterCommLib/ObjectSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalEndpointManager_InterCommLib
+{
+    public class ObjetSerializer
+    {
+        public static byte[] Serialize(object data)
+        {
+            using (var stream = new System.IO.MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, data);
+                return stream.ToArray();
+            }
+        }
+        public static T Deserialize<T>(byte[] data)
+        {
+            using (var stream = new System.IO.MemoryStream(data))
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+                return (T)serializer.Deserialize(stream);
+            }
+        }
+    }
+}
