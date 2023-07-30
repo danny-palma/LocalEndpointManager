d094310d3c88c9d5f662547b4298c4d7ab867a20
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-07-30 17:17:18 -0500

    Feat: Añadido interfaz de los modulos de comando
    Agregada la interfaz de los modulos de comandos

---
+++ LocalEndpointManager_InterCommLib/Interfaces/IModuleCommand.cs
@@ -0,0 +1,15 @@
+﻿using LocalEndpointManager_InterCommLib.MessageFormat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalEndpointManager_InterCommLib.Interfaces
+{
+    public interface IModuleCommand
+    {
+        string ModuleName { get; }
+        void Exec(MessageFormat.MessageFormat Args);
+    }
+}
