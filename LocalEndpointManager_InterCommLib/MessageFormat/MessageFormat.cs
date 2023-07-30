2d039cb34416115e129087d0c9cc5944aaa57fac
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-07-30 15:28:03 -0500

    Feat: Añadio los formatos
    Se agrego formatos de comunicacion para poder apliar la escalabilidad con modulos, enviar informacion y ejecutar comandos

---
+++ LocalEndpointManager_InterCommLib/MessageFormat/MessageFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalEndpointManager_InterCommLib.MessageFormat
+{
+    [Serializable]
+    public class MessageFormat
+    {
+        public string TypeMessage;
+        public byte[] Data;
+    }
+}
---
+++ LocalEndpointManager_InterCommLib/MessageFormat/SystemUpdateFormat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalEndpointManager_InterCommLib.MessageFormat
+{
+    [Serializable]
+    public class SystemUpdate
+    {
+        public string Client_Name;
+        public List<ProcesesInfo> Proceses;
+        public SystemUpdate(string Client_Name, List<ProcesesInfo> Proceses)
+        {
+            this.Client_Name = Client_Name;
+            this.Proceses = Proceses;
+        }
+    }
+    [Serializable]
+    public class ProcesesInfo
+    {
+        public string name;
+        public int PID;
+    }
+}
