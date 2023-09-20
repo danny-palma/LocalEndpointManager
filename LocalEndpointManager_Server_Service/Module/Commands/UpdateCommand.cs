df3f843d4f27c25a6c6f9c8d030dd31730aa4e56
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-09-20 17:41:30 -0500

    Se introdujo el modulo de update para actualizaciones de los procesos del cliente

---
+++ LocalEndpointManager_Server_Service/Module/Commands/UpdateCommand.cs
@@ -0,0 +1,20 @@
+﻿using LocalEndpointManager_InterCommLib.Interfaces;
+using LocalEndpointManager_InterCommLib.MessageFormat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalEndpointManager_Server_Service.Module.Commands
+{
+    internal class UpdateCommand : IModuleCommand
+    {
+        public string ModuleName => "Update";
+        public void Exec(MessageFormat Args)
+        {
+
+        }
+    }
+}
+    
