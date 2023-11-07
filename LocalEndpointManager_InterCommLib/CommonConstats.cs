ace38bc1f91bf30ad93e6448c004a7b33c16ba23
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-11-06 23:37:42 -0500

    chore: Añadido la constante de marca de agua para que pueda ser usado con otras entidades

--- LocalEndpointManager_InterCommLib/CommonConstats.cs
+++ LocalEndpointManager_InterCommLib/CommonConstats.cs
@@ -9,5 +9,6 @@ namespace LocalEndpointManager_InterCommLib
     public static class CommonConstats
     {
         public readonly static int BUFFER_SIZE = 16384; // 16 KB
+        public readonly static string WATER_MARK = $"Danny Palma Studios 2023 - {DateTime.Now.Year}";
     }
 }
