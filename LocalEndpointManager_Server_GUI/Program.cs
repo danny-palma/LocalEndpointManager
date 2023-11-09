1e3a710de61eb9af3b622956a185b513cd3b7ae3
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-11-08 19:35:26 -0500

    Feat(Dependences): added Font Awesome.Sharp dependence

--- LocalEndpointManager_Server_GUI/Program.cs
+++ LocalEndpointManager_Server_GUI/Program.cs
@@ -13,7 +13,7 @@ namespace LocalEndpointManager_Server_GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Window());
         }
     }
 }
