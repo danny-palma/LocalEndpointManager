39f29dff06b03441cff37a103ee8f9317be24f91
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-11-13 20:54:36 -0500

    Feat: initialized project

--- LocalEndpointManager_Server_GUI/Main_View.Designer.cs
+++ LocalEndpointManager_Server_GUI/Main_View.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LocalEndpointManager_Server_GUI
 {
-    partial class Form1
+    partial class Main_View
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_View));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- LocalEndpointManager_Server_GUI/Main_View.cs
+++ LocalEndpointManager_Server_GUI/Main_View.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LocalEndpointManager_Server_GUI
 {
-    public partial class Form1 : Form
+    public partial class Main_View : Form
     {
-        public Form1()
+        public Main_View()
         {
             InitializeComponent();
         }
