5457eb946ad8d45cebb9a12317379beb8fc8020a
Daniel Alejandro Palma Garcia <danielpalma2003@hotmail.com>
2023-11-20 20:28:55 -0500

    Chore: Created a pool to service conection

---
+++ LocalEndpointManager_Server_GUI/ServiceCommunicationPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalEndpointManager_Server_GUI
+{
+    public static class ServiceCommunicationPool
+    {
+        public static ServerService.CommunicationClient client = new ServerService.CommunicationClient();
+    }
+}
